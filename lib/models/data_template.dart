f4b85c19112d2fe9fb932ec2d42ab8f88a5af2ad
Anik <crystal.nik@gmail.com>
2024-02-13 01:06:45 +0600

    data class generic added

--- lib/models/data_template.dart
+++ lib/models/data_template.dart
@@ -2,6 +2,66 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'data_template.g.dart';
 part 'data_template.freezed.dart';
+
+class ApiResponse<T> {
+  bool success;
+  Meta meta;
+  List<T> data;
+
+  ApiResponse({
+    required this.success,
+    required this.meta,
+    required this.data,
+  });
+
+  factory ApiResponse.fromJson(Map<String, dynamic> json, T Function(dynamic) fromJsonT) {
+    return ApiResponse<T>(
+      success: json['success'],
+      meta: Meta.fromJson(json['meta']),
+      data: (json['data'] as List).map((itemJson) => fromJsonT(itemJson)).toList(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'success': success,
+      'meta': meta.toJson(),
+      // 'data': data.map((item) => item?.toJson()).toList(),
+    };
+  }
+}
+
+class Meta {
+  // Define your meta data fields here
+  Meta();
+
+  factory Meta.fromJson(Map<String, dynamic> json) {
+    // Parse and return Meta instance from JSON
+    return Meta();
+  }
+
+  Map<String, dynamic> toJson() {
+    // Convert Meta instance to JSON
+    return {};
+  }
+}
+
+class YourDataModel {
+  // Define your data model fields here
+  YourDataModel();
+
+  factory YourDataModel.fromJson(Map<String, dynamic> json) {
+    // Parse and return YourDataModel instance from JSON
+    return YourDataModel();
+  }
+
+  Map<String, dynamic> toJson() {
+    // Convert YourDataModel instance to JSON
+    return {};
+  }
+}
+
+
 //
 // class Template<T> {
 //   bool success;
@@ -20,20 +80,20 @@ part 'data_template.freezed.dart';
 //
 // }
 
-@freezed
-class Meta with _$Meta {
-  const factory Meta({required int statusCode, String? message}) = _Meta;
-
-  factory Meta.fromJson(Map<String, dynamic> json) => _$MetaFromJson(json);
-}
-
-@Freezed(genericArgumentFactories: true)
-sealed class ApiResponse<T> with _$ApiResponse<T> {
-  const factory ApiResponse({
-    required bool success,
-    required T data,
-    required Meta meta,
-}) = _ApiResponse;
-
-  factory ApiResponse.fromJson(Map<String, dynamic> json, T Function(Object?) fromJsonT) => _$ApiResponseFromJson(json, fromJsonT);
-}
+// @freezed
+// class Meta with _$Meta {
+//   const factory Meta({required int statusCode, String? message}) = _Meta;
+//
+//   factory Meta.fromJson(Map<String, dynamic> json) => _$MetaFromJson(json);
+// }
+//
+// @Freezed(genericArgumentFactories: true)
+// sealed class ApiResponse<T> with _$ApiResponse<T> {
+//   const factory ApiResponse({
+//     required bool success,
+//     required T data,
+//     required Meta meta,
+// }) = _ApiResponse;
+//
+//   factory ApiResponse.fromJson(Map<String, dynamic> json, T Function(Object?) fromJsonT) => _$ApiResponseFromJson(json, fromJsonT);
+// }

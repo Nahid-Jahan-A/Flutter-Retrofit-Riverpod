bb64cb5da355af04fa2f9021c665622b2f8282a4
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-02-15 15:57:27 +0600

    conflicts_resolved

--- lib/screen/groups_screen.dart
+++ lib/screen/groups_screen.dart
@@ -21,7 +21,7 @@ class _GroupScreen extends ConsumerState<GroupScreen> {
   final TextEditingController _groupNameController = TextEditingController();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Groups"),
@@ -48,147 +48,147 @@ class _GroupScreen extends ConsumerState<GroupScreen> {
       ),
     );
   }
-}
 
 
-
-void _showCreatePostDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Create Group'),
-        content: TextField(
-          controller: _groupNameController,
-          decoration: const InputDecoration(labelText: 'Enter group name'),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Cancel'),
+  void _showCreatePostDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Create Group'),
+          content: TextField(
+            controller: _groupNameController,
+            decoration: const InputDecoration(labelText: 'Enter group name'),
           ),
-          ElevatedButton(
-            onPressed: () async {
-              Logger logger = Logger();
-              final text = _groupNameController.text.trim();
-              if (text.isNotEmpty) {
-                final groupState = ref.read(groupNotifierProvider.notifier);
-                await groupState.createNewGroup(groupName: text);
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                Logger logger = Logger();
+                final text = _groupNameController.text.trim();
+                if (text.isNotEmpty) {
+                  final groupState = ref.read(groupNotifierProvider.notifier);
+                  await groupState.createNewGroup(groupName: text);
+                  logger.i(
+                      "Value of group status -----> ${ref
+                          .read(groupNotifierProvider)
+                          .status == GroupStatus.created}");
+                  logger.i("Context Mounted?-----------> ${context.mounted}");
+                  if (context.mounted &&
+                      ref
+                          .read(groupNotifierProvider)
+                          .status ==
+                          GroupStatus.created) {
+                    Navigator.pop(context);
+                    groupState.fetchGroupData();
+                  }
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Please enter group name')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildContent(GroupState state) {
+    switch (state.status) {
+      case GroupStatus.loading:
+        return const Center(child: CircularProgressIndicator());
+      case GroupStatus.loaded:
+        return ListView.builder(
+          itemCount: state.data.length,
+          itemBuilder: (context, index) {
+            final group = state.data[index];
+            return ListTile(
+              onTap: () async {
+                Logger logger = Logger();
+                final groupState = ref.watch(groupNotifierProvider.notifier);
+                logger.i("Single group ID -------> ${group.id.toString()}");
+                await groupState.getGroupById(group.id.toString());
+                logger.i("Is Mounted------> ${context.mounted}");
                 logger.i(
-                    "Value of group status -----> ${ref
-                        .read(groupNotifierProvider)
-                        .status == GroupStatus.created}");
-                logger.i("Context Mounted?-----------> ${context.mounted}");
+                    "Group state status------> ${ref
+                        .watch(groupNotifierProvider)
+                        .status}");
+                if (ref
+                    .watch(groupNotifierProvider)
+                    .status ==
+                    GroupStatus.loadedSingle) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          SingleGroupScreen(
+                            group: group,
+                          ),
+                    ),
+                  );
+                }
+              },
+              title: Text(group.name),
+              subtitle: Text(group.status.toString()),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () =>
+                    _showDeleteConfirmationDialog(context, group.id),
+              ),
+            );
+          },
+        );
+      case GroupStatus.error:
+        return Center(child: Text('Error: ${state.error}'));
+      default:
+        return const Center(child: Text('No data available'));
+    }
+  }
+
+  Future<void> _showDeleteConfirmationDialog(BuildContext context, id) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Confirm Deletion'),
+          content: const Text('Are you sure you want to delete this group?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // final groupState = ref.read(groupNotifierProvider.notifier);
+                // groupState.deleteGroupById(id);
+                debugPrint("DELETE_ID $id");
+                final groupState = ref.read(groupNotifierProvider.notifier);
+                await groupState.deleteGroupById(id.toString());
                 if (context.mounted &&
                     ref
                         .read(groupNotifierProvider)
                         .status ==
-                        GroupStatus.created) {
+                        GroupStatus.deleted) {
                   Navigator.pop(context);
-                  groupState.fetchGroupData();
+                  await groupState.fetchGroupData();
                 }
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Please enter group name')),
-                );
-              }
-            },
-            child: const Text('Submit'),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-Widget _buildContent(GroupState state) {
-  switch (state.status) {
-    case GroupStatus.loading:
-      return const Center(child: CircularProgressIndicator());
-    case GroupStatus.loaded:
-      return ListView.builder(
-        itemCount: state.data.length,
-        itemBuilder: (context, index) {
-          final group = state.data[index];
-          return ListTile(
-            onTap: () async {
-              Logger logger = Logger();
-              final groupState = ref.watch(groupNotifierProvider.notifier);
-              logger.i("Single group ID -------> ${group.id.toString()}");
-              await groupState.getGroupById(group.id.toString());
-              logger.i("Is Mounted------> ${context.mounted}");
-              logger.i(
-                  "Group state status------> ${ref
-                      .watch(groupNotifierProvider)
-                      .status}");
-              if (ref
-                  .watch(groupNotifierProvider)
-                  .status ==
-                  GroupStatus.loadedSingle) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        SingleGroupScreen(
-                          group: group,
-                        ),
-                  ),
-                );
-              }
-            },
-            title: Text(group.name),
-            subtitle: Text(group.status.toString()),
-            trailing: IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: () =>
-                  _showDeleteConfirmationDialog(context, group.id),
+              },
+              child: const Text('Delete'),
             ),
-          );
-        },
-      );
-    case GroupStatus.error:
-      return Center(child: Text('Error: ${state.error}'));
-    default:
-      return const Center(child: Text('No data available'));
+          ],
+        );
+      },
+    );
   }
-}
 
-Future<void> _showDeleteConfirmationDialog(BuildContext context, id) async {
-  return showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text('Confirm Deletion'),
-        content: const Text('Are you sure you want to delete this group?'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Cancel'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              // final groupState = ref.read(groupNotifierProvider.notifier);
-              // groupState.deleteGroupById(id);
-              debugPrint("DELETE_ID $id");
-              final groupState = ref.read(groupNotifierProvider.notifier);
-              await groupState.deleteGroupById(id.toString());
-              if (context.mounted &&
-                  ref
-                      .read(groupNotifierProvider)
-                      .status ==
-                      GroupStatus.deleted) {
-                Navigator.pop(context);
-                await groupState.fetchGroupData();
-              }
-            },
-            child: const Text('Delete'),
-          ),
-        ],
-      );
-    },
-  );
 }

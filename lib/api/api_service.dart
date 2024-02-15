7f755e7498d6907e43920ca384c59cd7b07e79be
Nahid Anik <nahidjahan.a@utkorsho.org>
2024-02-15 16:10:18 +0600

    constant_url_file_created

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,38 +1,13 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_retrofit/models/group_data.dart';
-import 'package:flutter_retrofit/models/auth_data.dart';
 import 'package:flutter_retrofit/model_practice/post.dart';
+import 'package:flutter_retrofit/models/auth_data.dart';
 import 'package:flutter_retrofit/models/class.dart';
+import 'package:flutter_retrofit/models/group_data.dart';
 import 'package:retrofit/http.dart';
 
+import '../util/apis.dart';
+
 part 'api_service.g.dart';
-
-class Apis {
-  static const String posts = "/posts";
-
-  static const String POSTS_BASE_URL = "https://jsonplaceholder.typicode.com";
-
-  static const String UTKORSHO_BASE_URL = "http://10.10.10.31:38080";
-
-
-  static const String class_endpoint = "/api/v1/classes";
-
-  
-  static const String UTKORSHO_BASE_URL_CS = "http://10.10.10.31:28089/course-service/";
-
-  static const String auth_endpoint = "/auth/login"; //post method requires login id and password
-
-  static const String group_endpoint = "api/v1/groups";
-
-  static const String add_group_endpoint = "api/v1/groups"; //post method requires group name as payload
-
-
-  static const String delete_group_endpoint = "api/v1/groups/{groupId}"; //post method requires group id as payload
-
-  static const String get_single_group_endpoint = "api/v1/groups/{groupId}"; //post method requires group id as payload
-
-  static const String test_endpoint = "/api/v1/hello"; //get method requires access token in the header
-}
 
 @RestApi(baseUrl: Apis.POSTS_BASE_URL)
 abstract class ApiClient {
---
+++ lib/util/apis.dart
@@ -0,0 +1,26 @@
+class Apis {
+  static const String posts = "/posts";
+
+  static const String POSTS_BASE_URL = "https://jsonplaceholder.typicode.com";
+
+  static const String UTKORSHO_BASE_URL = "http://10.10.10.31:38080";
+
+
+  static const String class_endpoint = "/api/v1/classes";
+
+
+  static const String UTKORSHO_BASE_URL_CS = "http://10.10.10.31:28089/course-service/";
+
+  static const String auth_endpoint = "/auth/login"; //post method requires login id and password
+
+  static const String group_endpoint = "api/v1/groups";
+
+  static const String add_group_endpoint = "api/v1/groups"; //post method requires group name as payload
+
+
+  static const String delete_group_endpoint = "api/v1/groups/{groupId}"; //post method requires group id as payload
+
+  static const String get_single_group_endpoint = "api/v1/groups/{groupId}"; //post method requires group id as payload
+
+  static const String test_endpoint = "/api/v1/hello"; //get method requires access token in the header
+}
